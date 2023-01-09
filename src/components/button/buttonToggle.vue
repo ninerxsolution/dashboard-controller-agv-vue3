63171f49399d67fef7c18e96e498b65db16d9715
ninerxsolution <ninerxsolution@gmail.com>
2023-01-09 23:58:21 +0700

    finish slide-2

---
+++ src/components/button/buttonToggle.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="">
+        <div v-if="btn_is_clicked"></div>
+        <div ref="btn" class="toggle-btn" @click="btn_click">
+            <div class="inner-circle"></div>
+        </div>
+    </div>
+</template>
+<script>
+export default{
+    name:'buttonToggle',
+    data(){
+        return{
+            btn_is_clicked:false,
+        }
+    },
+    methods:{
+        btn_click(){
+            if(this.btn_is_clicked == false){
+                this.$refs.btn.className = 'toggle-btn active';
+            }else{
+                this.$refs.btn.className = 'toggle-btn';
+            }
+            this.btn_is_clicked = !this.btn_is_clicked;
+        }
+    }
+}
+</script>
+
+<style>
+
+.toggle-btn{
+    width: 40px;
+    height:20px;
+    background: gray;
+    border-radius: 30px;
+    padding: 1px;
+    transition: all 200ms ease-in-out;
+}
+.toggle-btn > .inner-circle{
+    background-color: #fff;
+    height: 100%;
+    width:50%;
+    border-radius: 50%;
+    transition: all 200ms ease-in-out;
+}
+
+.toggle-btn.active{
+    background: #00acee;
+}
+.toggle-btn.active > .inner-circle{
+
+    margin-left: 50%;
+}
+</style>
--- src/components/swiper/slides/slide_monitor.vue
+++ src/components/swiper/slides/slide_monitor.vue
@@ -193,21 +193,265 @@
             <div class="col-12 grid-margin">
                 <div class="card mb-4">
                     <div class="card-body">
-                        <div class="col-12 d-flex justify-content-between">
-                            <h5 class="d-flex">
+
+                        <div class="d-flex justify-content-between">
+                            <h5 class="d-flex mb-0 mt-1">
                                 MAP FOR ROUTE
                             </h5>
-                            <div class="">
-                                hi
+                            <div class="d-flex">
+                                <button class="btn-dark text-warning">
+                                    <small>
+                                        Non drawback AGV 1
+                                    </small>
+                                </button>
+                                <i class="mdi mdi-eye px-2"></i>
+                                <button class="btn-dark text-success">
+                                    <small>
+                                        Battery AGV 1 OK
+                                    </small>
+                                </button>
+                                <i class="mdi mdi-truck px-2"></i>
                             </div>
-                            
-                            <div>
-                                hi
+
+                            <div class="d-flex">
+                                <div class="text">
+                                    CONNECT :
+                                </div>
+                                <button-toggle class="mx-2 mt-1" />
+                                <button class="btn btn-warning text-dark mx-2 px-4">
+                                    UPDATE MAP
+                                </button>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-12 text-center">
+                            <div class="col-12 mt-2">
+                                <div class="card-body bg-dark p-2">
+                                    <div class="card-title d-flex justify-content-between">
+                                        <small>
+                                            Point Station
+                                        </small>
+                                        <small>
+                                            Point End
+                                        </small>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <ul>
+                                            <li>
+                                                AGV 1
+                                            </li>
+                                        </ul>
+                                        <div class="progress mt-1 mx-3" style="width:85%;">
+                                            <div class="progress-bar bg-warning" style="width:47%;">
+                                                <div class="text-dark">
+                                                    AGV 1 Running : SPEED 0.5 m/s
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <ul>
+                                            <li>
+                                                AGV 2
+                                            </li>
+                                        </ul>
+                                        <div class="progress mt-1 mx-3" style="width:85%;">
+                                            <div class="progress-bar bg-warning" style="width:0%;">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-between">
+                                        <ul>
+                                            <li>
+                                                AGV 3
+                                            </li>
+                                        </ul>
+                                        <div class="progress mt-1 mx-3" style="width:85%;">
+                                            <div class="progress-bar bg-warning" style="width:0%;">
 
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-between">
+                                <div class="col-2 card-body bg-dark mt-2">
+                                    <div class="d-flex">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h6">
+                                                    AGV 1
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-success">
+                                            <div class="small mt-1 mx-2">
+                                                Running
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h6">
+                                                    AGV 2
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-danger">
+                                            <div class="small mt-1 mx-2">
+                                                Offline
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h6">
+                                                    AGV 3
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-danger">
+                                            <div class="small mt-1 mx-2">
+                                                Offline
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-2 card-body bg-dark mt-2 pt-4">
+                                    <div class="d-flex">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h6">
+                                                    AGV 1
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-primary">
+                                            <div class="small mt-1 mx-2">
+                                                200 ton
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h6">
+                                                    AGV 2
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-warning">
+                                            <div class="small mt-1 mx-2">
+                                                0 ton
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h6">
+                                                    AGV 3
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-warning">
+                                            <div class="small mt-1 mx-2">
+                                                0 ton
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-2 card-body bg-dark mt-2 pt-4">
+                                    <div class="d-flex">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h6">
+                                                    AGV 1
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-primary">
+                                            <div class="small mt-1 mx-2">
+                                                200 ton
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h6">
+                                                    AGV 2
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-warning">
+                                            <div class="small mt-1 mx-2">
+                                                0 ton
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h6">
+                                                    AGV 3
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-warning">
+                                            <div class="small mt-1 mx-2">
+                                                0 ton
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="col-3 card-body bg-dark mt-2">
+                                    <div class="d-flex text-warning justify-content-between">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h5">
+                                                    DISTANCE TOTAL
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-right">
+                                            <div class="small mt-1 mx-2">
+                                                500 m
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex text-primary justify-content-between">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h5">
+                                                    MAXIMUM CYCLE
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-right">
+                                            <div class="small mt-1 mx-2">
+                                                10 min
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex text-warning justify-content-between">
+                                        <ul class="mb-0">
+                                            <li>
+                                                <div class="h5">
+                                                    MAXIMUX SPEED
+                                                </div>
+                                            </li>
+                                        </ul>
+                                        <div class="text-right">
+                                            <div class="small mt-1 mx-2">
+                                                2 m/s
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -220,11 +464,13 @@
 <script>
 import "vue3-circle-progress/dist/circle-progress.css";
 import CircleProgress from "vue3-circle-progress";
+import buttonToggle from '@/components/button/buttonToggle.vue';
 
 export default {
     name: 'App',
     components: {
         CircleProgress,
+        buttonToggle,
     }
 }
 </script>
