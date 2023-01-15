5d096f499bd34674af484aa1088fb116503ee2e8
ninerxsolution <ninerxsolution@gmail.com>
2023-01-15 13:37:28 +0700

    pulling another branch

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Dashboard></Dashboard>
     อันนี้แบบว่าทดสอบนะว่ามันจะเป็นยังไง
     อันนี้ทดสอบ Pulling fetch นะะะะ โหล้ๆ
+    ทดสอบอีกรอบนะ อันนี้ commit มาจาก branch ชื่ออื่น ๆ
   </div>
 </template>
 
